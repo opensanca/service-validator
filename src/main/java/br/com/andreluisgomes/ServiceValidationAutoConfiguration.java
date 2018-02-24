9deff9168944cdb4248be203d85b44b9c6854528
André Luis Gomes <s2it_agomes@uolinc.com>
2018-02-24 11:53:41 -0300

    Add ServiceValidationAutoConfiguration for Spring Boot aplications

---
+++ src/main/java/br/com/andreluisgomes/ServiceValidationAutoConfiguration.java
@@ -0,0 +1,9 @@
+package br.com.andreluisgomes;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("br.com.andreluisgomes")
+public class ServiceValidationAutoConfiguration {
+}
